d665bc452c9cecfb85bd192983214de5f5b7bb04
Danail Branekov <danailster@gmail.com>
2021-06-09 12:24:30 +0300

    Fix cleanup script
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>
    Issue: cloudfoundry-incubator/eirini-release#217

--- deployment/scripts/cleanup.sh
+++ deployment/scripts/cleanup.sh
@@ -7,16 +7,14 @@ readonly SYSTEM_NAMESPACE=eirini-controller
 
 source "$SCRIPT_DIR/helpers/print.sh"
 
-delete_eirini() {
-  helm --namespace "$SYSTEM_NAMESPACE" delete eirini || true
-  helm --namespace "$SYSTEM_NAMESPACE" delete nats || true
+delete_eirini_controller() {
+  helm --namespace "$SYSTEM_NAMESPACE" delete eirini-controller || true
   helm --namespace "$SYSTEM_NAMESPACE" delete prometheus || true
-  kubectl delete -f "$SCRIPT_DIR/assets/wiremock.yml" || true
 }
 
 main() {
   print_disclaimer
-  delete_eirini
+  delete_eirini_controller
 }
 
 main
