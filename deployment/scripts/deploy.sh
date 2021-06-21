a32fb29c7f1cd8234b34d8a8106fd6b4d147a299
Danail Branekov <danailster@gmail.com>
2021-06-21 14:59:10 +0000

    Extract the deploy-only step
    Issue: https://github.com/cloudfoundry-incubator/eirini-ci/issues/18
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -5,51 +5,5 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 SCRIPT_DIR="$ROOT_DIR/deployment/scripts"
 
-export KUBECONFIG
-KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
-KUBECONFIG=$(readlink -f "$KUBECONFIG")
-
-export GOOGLE_APPLICATION_CREDENTIALS
-GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS:-""}
-if [[ -n $GOOGLE_APPLICATION_CREDENTIALS ]]; then
-  GOOGLE_APPLICATION_CREDENTIALS=$(readlink -f "$GOOGLE_APPLICATION_CREDENTIALS")
-fi
-
-readonly SYSTEM_NAMESPACE=eirini-controller
-
-source "$SCRIPT_DIR/helpers/print.sh"
-
-main() {
-  print_disclaimer
-  "$ROOT_DIR"/deployment/scripts/build.sh
-  generate_secrets
-  install_prometheus
-  install_eirini_controller "$@"
-}
-
-generate_secrets() {
-  "$SCRIPT_DIR/generate-secrets.sh" "*.${SYSTEM_NAMESPACE}.svc"
-}
-
-install_prometheus() {
-  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-  helm repo update
-  helm upgrade prometheus \
-    --install prometheus-community/prometheus \
-    --namespace "$SYSTEM_NAMESPACE" \
-    --wait
-}
-
-install_eirini_controller() {
-  local resource_validator_ca_bundle
-  resource_validator_ca_bundle="$(kubectl get secret -n $SYSTEM_NAMESPACE eirini-resource-validator-certs -o jsonpath="{.data['tls\.ca']}")"
-  helm upgrade eirini-controller \
-    --install "$ROOT_DIR/deployment/helm" \
-    --namespace "$SYSTEM_NAMESPACE" \
-    --values "$SCRIPT_DIR/assets/value-overrides.yaml" \
-    --set "resource_validator_ca_bundle=$resource_validator_ca_bundle" \
-    --wait \
-    "$@"
-}
-
-main "$@"
+"$SCRIPT_DIR"/build.sh
+"$SCRIPT_DIR"/deploy-only.sh "$@"
