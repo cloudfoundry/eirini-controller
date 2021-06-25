ea5589b0f4feff20492e3ecf1b2a9697e2f18b42
Georgi Sabev <georgethebeatle@gmail.com>
2021-06-25 15:36:12 +0100

    Fix counterfeiter invocation not to use the binary
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>
    Issue: cloudfoundry-incubator/eirini-release#217

--- k8s/package.go
+++ k8s/package.go
@@ -2,5 +2,5 @@ package k8s
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -generate
 
-//go:generate counterfeiter sigs.k8s.io/controller-runtime/pkg/client.Client
-//go:generate counterfeiter sigs.k8s.io/controller-runtime/pkg/client.StatusWriter
+//counterfeiter:generate sigs.k8s.io/controller-runtime/pkg/client.Client
+//counterfeiter:generate sigs.k8s.io/controller-runtime/pkg/client.StatusWriter
