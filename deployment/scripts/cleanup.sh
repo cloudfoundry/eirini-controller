9b724c38ad057b270febb8138ed9795686a054f9
Georgi Sabev <georgethebeatle@gmail.com>
2021-06-28 13:41:34 +0000

    Also delete system namespace to truly cleanup promotheus
    We've been seeing errors where a persistent volume claim is being
    deleted which stops prometheus from deploying. Hope is that by
    destroying the namespace we'll avoid that problem.
    
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- deployment/scripts/cleanup.sh
+++ deployment/scripts/cleanup.sh
@@ -10,6 +10,7 @@ source "$SCRIPT_DIR/helpers/print.sh"
 delete_eirini_controller() {
   helm --namespace "$SYSTEM_NAMESPACE" delete eirini-controller || true
   helm --namespace "$SYSTEM_NAMESPACE" delete prometheus || true
+  kubectl delete namespace "$SYSTEM_NAMESPACE" --wait || true
 }
 
 main() {
