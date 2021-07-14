c61b22baaef27e6ea6d4e8c69349557d4b0e3324
Mario Nitchev <marionitchev@gmail.com>
2021-07-14 15:07:45 +0300

    Move everything in a single binary
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>
    Issue: cloudfoundry-incubator/eirini-controller#13

--- scripts/check-everything.sh
+++ scripts/check-everything.sh
@@ -19,7 +19,7 @@ ensure_kind_cluster() {
   local cluster_name
   cluster_name="$1"
   if ! kind get clusters | grep -q "$cluster_name"; then
-    current_cluster="$(kubectl config current-context)" || true
+    current_cluster="$(KUBECONFIG="$HOME/.kube/config" kubectl config current-context)" || true
     kindConfig=$(mktemp)
     cat <<EOF >>"$kindConfig"
 kind: Cluster
@@ -30,9 +30,11 @@ EOF
     flock -x "$clusterLock" kind create cluster --name "$cluster_name" --config "$kindConfig" --wait 5m
     rm -f "$kindConfig"
     if [[ -n "$current_cluster" ]]; then
-      kubectl config use-context "$current_cluster"
+      KUBECONFIG="$HOME/.kube/config" kind export kubeconfig --name "$cluster_name"
+      KUBECONFIG="$HOME/.kube/config" kubectl config use-context "$current_cluster"
     fi
   fi
+
   kind export kubeconfig --name "$cluster_name" --kubeconfig "$HOME/.kube/$cluster_name.yml"
 }
 
@@ -143,16 +145,14 @@ redeploy_prometheus() {
 redeploy_eirini_controller() {
   render_dir=$(mktemp -d)
   trap "rm -rf $render_dir" EXIT
-  env_injector_ca_bundle="$(kubectl get secret -n eirini-controller eirini-instance-index-env-injector-certs -o jsonpath="{.data['tls\.ca']}")"
-  ca_bundle="$(kubectl get secret -n eirini-controller eirini-resource-validator-certs -o jsonpath="{.data['tls\.ca']}")"
+  webhooks_ca_bundle="$(kubectl get secret -n eirini-controller eirini-webhooks-certs -o jsonpath="{.data['tls\.ca']}")"
   kbld -f "$EIRINI_CONTROLLER_DIR/scripts/kbld.yml" \
     -f "$EIRINI_CONTROLLER_DIR/deployment/helm/values-template.yaml" \
     >"$EIRINI_CONTROLLER_DIR/deployment/helm/values.yaml"
 
   "$EIRINI_CONTROLLER_DIR/deployment/scripts/render-templates.sh" eirini-controller "$render_dir" \
     --values "$EIRINI_CONTROLLER_DIR/deployment/scripts/assets/value-overrides.yaml" \
-    --set "webhook_ca_bundle=$env_injector_ca_bundle" \
-    --set "resource_validator_ca_bundle=$ca_bundle"
+    --set "webhooks.ca_bundle=$webhooks_ca_bundle"
   for img in $(grep -oh "kbld:.*" "$EIRINI_CONTROLLER_DIR/deployment/helm/values.yaml"); do
     kind load docker-image --name eats "$img"
   done
--- tests/integration/cmd/eirini_controller_test.go
+++ tests/integration/cmd/eirini_controller_test.go
@@ -1,12 +1,15 @@
 package cmd_test
 
 import (
+	"fmt"
+	"net"
 	"os"
 
 	eirinictrl "code.cloudfoundry.org/eirini-controller"
 	"code.cloudfoundry.org/eirini-controller/tests/integration"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
@@ -36,5 +39,26 @@ var _ = Describe("EiriniController", func() {
 
 	It("should be able to start properly", func() {
 		Consistently(session).ShouldNot(gexec.Exit())
+	})
+
+	When("the config file doesn't exist", func() {
+		It("exits reporting missing config file", func() {
+			session = eiriniBins.EiriniController.Restart("/does/not/exist", session)
+			Eventually(session).Should(gexec.Exit())
+			Expect(session.ExitCode).ToNot(BeZero())
+			Expect(session.Err).To(gbytes.Say("Failed to read config file: failed to read file"))
+		})
+	})
+
+	Describe("Webhooks", func() {
+		It("runs the webhook service and registers it", func() {
+			Eventually(func() error {
+				_, err := net.Dial("tcp", fmt.Sprintf(":%d", config.WebhookPort))
+
+				return err
+			}).Should(Succeed())
+
+			Consistently(session).ShouldNot(gexec.Exit())
+		})
 	})
 })
