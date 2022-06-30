b7d08b341cb10213371f8f0b1c14b7a2333f24da
Danail Branekov <danailster@gmail.com>
2022-06-30 13:54:18 +0000

    Reintroduce CRD generation
    This was accidently removed in yesterday's refactor
    
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- hack/run-code-generator.sh
+++ hack/run-code-generator.sh
@@ -27,3 +27,14 @@ go run k8s.io/code-generator/cmd/client-gen \
   "$EIRINI_CONTROLLER_ROOT/pkg/apis/eirini/v1"
 
 cp -R "$EIRINI_CONTROLLER_ROOT"/code.cloudfoundry.org/eirini-controller/pkg/* "$EIRINI_CONTROLLER_ROOT"/pkg/
+
+# CRD Generation
+
+EIRINI_TMP_CRD="$EIRINI_CONTROLLER_ROOT/code.cloudfoundry.org/crds"
+mkdir -p "$EIRINI_TMP_CRD"
+go run sigs.k8s.io/controller-tools/cmd/controller-gen \
+  crd \
+  output:dir="$EIRINI_TMP_CRD" \
+  paths="$EIRINI_CONTROLLER_ROOT"/pkg/apis/...
+cp "$EIRINI_TMP_CRD/eirini.cloudfoundry.org_lrps.yaml" "$EIRINI_CONTROLLER_ROOT/deployment/helm/templates/core/lrp-crd.yml"
+cp "$EIRINI_TMP_CRD/eirini.cloudfoundry.org_tasks.yaml" "$EIRINI_CONTROLLER_ROOT/deployment/helm/templates/core/task-crd.yml"
